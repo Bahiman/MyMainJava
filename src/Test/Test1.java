73e9d809ea4e38cd2d6b523047b3f60b81dd478b
Bahiman <99581000+Bahiman@users.noreply.github.com>
2022-04-18 18:56:53 +0400

    Hello

--- src/Test/Test1.java
+++ src/Test/Test1.java
@@ -4,5 +4,6 @@ public class Test1 {
     public static void main(String[] args) {
         System.out.println("Hello0 World");
         System.out.println("What does the fox sayf");
+        System.out.println("Nigers");
     }
 }
