0eacccb14100eb62e18951e89c5580968669b8f3
Bahiman <99581000+Bahiman@users.noreply.github.com>
2022-04-18 18:54:42 +0400

    Hello

---
+++ src/Test/Test1.java
@@ -0,0 +1,8 @@
+package Test;
+
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("Hello0 World");
+        System.out.println("What does the fox sayf");
+    }
+}
