ce6891bcf9ce9315db5cb19dcccfcf8c919b70fe
bahge <bahgez14032009@gmail.com>
2022-04-20 22:45:39 +0400

    Hello

--- src/Homework_1/Main.java
+++ src/Homework_1/Main.java
@@ -2,17 +2,16 @@ package Homework_1;
 
 public class Main {
     public static void main(String[] args) {
-//        int random = (int) (Math.random() * 200 + 50);
-//
-//        System.out.println(random);
-//
-//        Sub.task_one();
-//
-//        Sub.task_two();
+        int random = (int) (Math.random() * 200 + 50);
+
+        System.out.println(random);
+
+        Sub.task_one();
+
+        Sub.task_two();
 
         Info info_one = new Info("name", "33", "Hasan Salmani");
         System.out.println(info_one);
-
 
     }
 }
